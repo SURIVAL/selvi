fa94e1601af28932b6d5f69e380bd6bab2b1cc6e
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-02-28 16:07:03 +0530

    Update and rename 78.c to 72.c

---
+++ 72.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+ int str[24],n,flag=0;
+ printf("enter a string");
+ scanf("%s",&str[n]);
+ while(str[n]!=" ")
+ {
+    if(str[n]=="a"||str[n]=="e"||str[n]=="i"||str[n]=="o"||str[n]=="u")
+    { 
+     flag=1;
+     i++;
+    printf("yes");
+    }
+    else{
+    printf("no");
+    }
+    
+    
