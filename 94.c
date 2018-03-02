7517295ae977f12ead41460b4324c86cef6f684c
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-03-02 16:10:48 +0530

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,k,i;
+    printf("enter the n and k value");
+    scanf("%d",&n , &k);
+    for(i=0;i>n;i++)
+    if(n>1)
+    {
+        printf("%d",i);
+        
+    }
+    printf("%d",k);
+}
