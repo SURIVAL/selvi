bbd7f9951ea926109420f5f956753e02f2d002b0
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-03-02 16:21:23 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int celsius,kelvin;
+    printf("enter the celsius value");
+	scanf("%d",&celsius);
+	kelvin=celsius+273;
+	printf("%d",kelvin);
+	return 0;
+}
